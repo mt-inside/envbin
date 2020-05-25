1dc254dfd089fb2bac8edf8756d6fdc5c6561259
Matt Turner <matturner@gmail.com>
2020-05-25 18:47:14 +0100

    Print api handler messages to stdout too

--- pkg/handlers/api.go
+++ pkg/handlers/api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/mt-inside/envbin/pkg/actions"
 	"github.com/mt-inside/envbin/pkg/data"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -16,14 +17,19 @@ func HandleApi(apiMux *mux.Router) {
 	apiMux.NotFoundHandler = notFound(apiMux)
 
 	apiMux.HandleFunc("/exit", exit).Methods("GET")
-	apiMux.HandleFunc("/delay", delay).Methods("GET") /* Latency to first byte */
+	apiMux.HandleFunc("/delay", delay).Methods("GET")         /* Latency to first byte */
 	apiMux.HandleFunc("/bandwidth", bandwidth).Methods("GET") /* Latency between bytes */
 	apiMux.HandleFunc("/errorrate", errorrate).Methods("GET") /* Proportion of 500s */
-	apiMux.HandleFunc("/allocate", allocate).Methods("GET") /* Allocate (and use) memory */
-	apiMux.HandleFunc("/free", free).Methods("GET") /* Free all the extra memory */
-	apiMux.HandleFunc("/cpu", cpu).Methods("GET") /* Use CPU at a given rate */
+	apiMux.HandleFunc("/allocate", allocate).Methods("GET")   /* Allocate (and use) memory */
+	apiMux.HandleFunc("/free", free).Methods("GET")           /* Free all the extra memory */
+	apiMux.HandleFunc("/cpu", cpu).Methods("GET")             /* Use CPU at a given rate */
 	apiMux.HandleFunc("/liveness", liveness).Methods("GET")
 	apiMux.HandleFunc("/readiness", readiness).Methods("GET")
+}
+
+func inform(msg string, w http.ResponseWriter) {
+	fmt.Fprintf(w, msg)
+	log.Printf(msg)
 }
 
 func notFound(apiMux *mux.Router) http.HandlerFunc {
@@ -51,19 +57,19 @@ func exit(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, "%v\n", err)
 	} else {
-		fmt.Fprintf(w, "Exiting %d\n", rc)
+		inform(fmt.Sprintf("Exiting %d\n", rc), w)
 		w.(http.Flusher).Flush()
 		os.Exit(int(rc))
 	}
 }
 
-func delay (w http.ResponseWriter, r *http.Request) {
+func delay(w http.ResponseWriter, r *http.Request) {
 	d, err := strconv.ParseInt(r.URL.Query().Get("value"), 0, 64)
 	if err != nil {
 		fmt.Fprintf(w, "%v\n", err)
 	} else {
 		data.SetDelay(d)
-		fmt.Fprintf(w, "Delay set to %v\n", d)
+		inform(fmt.Sprintf("Delay set to %v\n", d), w)
 	}
 }
 
@@ -73,7 +79,7 @@ func bandwidth(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%v\n", err)
 	} else {
 		data.SetBandwidth(b)
-		fmt.Fprintf(w, "Bandwidth set to %s/s\n", units.BytesSize(float64(b)))
+		inform(fmt.Sprintf("Bandwidth set to %s/s\n", units.BytesSize(float64(b))), w)
 	}
 }
 
@@ -83,7 +89,7 @@ func errorrate(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%v\n", err)
 	} else {
 		data.SetErrorRate(e)
-		fmt.Fprintf(w, "Error rate set to %v\n", e)
+		inform(fmt.Sprintf("Error rate set to %v\n", e), w)
 	}
 }
 
@@ -92,7 +98,7 @@ func allocate(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, "%v\n", err)
 	} else {
-		fmt.Fprintf(w, "Allocating %s bytes\n", units.BytesSize(float64(a)))
+		inform(fmt.Sprintf("Allocating %s bytes\n", units.BytesSize(float64(a))), w)
 		actions.AllocAndTouch(a)
 	}
 }
@@ -108,7 +114,7 @@ func cpu(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%v\n", err)
 	} else {
 		data.SetCPUUse(c)
-		fmt.Fprintf(w, "CPU usage set to %v\n", c)
+		inform(fmt.Sprintf("CPU usage set to %v\n", c), w)
 	}
 }
 
@@ -118,7 +124,7 @@ func liveness(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%v\n", err)
 	} else {
 		data.SetLiveness(l)
-		fmt.Fprintf(w, "Liveness check set to %v\n", l)
+		inform(fmt.Sprintf("Liveness check set to %v\n", l), w)
 	}
 }
 
@@ -128,6 +134,6 @@ func readiness(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%v\n", err)
 	} else {
 		data.SetReadiness(ready)
-		fmt.Fprintf(w, "Readiness check set to %v\n", ready)
+		inform(fmt.Sprintf("Readiness check set to %v\n", ready), w)
 	}
 }

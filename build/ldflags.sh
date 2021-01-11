c059699af01e5353032d7b8bb1ddff5a6deacd81
Matt Turner <matturner@gmail.com>
2021-01-11 21:54:40 +0000

    Improve build scripts

--- build/ldflags.sh
+++ build/ldflags.sh
@@ -1,4 +1,3 @@
 version=$(git describe --tags --dirty)
-gitCommit=$(git rev-parse --short HEAD)
-buildTime=$(date +%Y-%m-%d_%H:%M:%S%z)
-echo "-X github.com/mt-inside/envbin/pkg/data.Version=${version} -X github.com/mt-inside/envbin/pkg/data.GitCommit=${gitCommit} -X github.com/mt-inside/envbin/pkg/data.BuildTime=${buildTime}"
+buildTime=$(date +"%F %T%Z")
+echo -X "'"github.com/mt-inside/envbin/pkg/data.Version=${version}"'" -X "'"github.com/mt-inside/envbin/pkg/data.BuildTime=${buildTime}"'"

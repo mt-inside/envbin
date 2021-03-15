7d5daa5fd87a81eebb0ee2ccaeb7b52e964f06fc
Matt Turner <user.personal@users.noreply.github.com>
2021-03-15 19:15:14 +0000

    CPU cache info

--- pkg/data/hardware.go
+++ pkg/data/hardware.go
@@ -5,6 +5,7 @@ import (
 	"runtime"
 	"strconv"
 
+	"github.com/docker/go-units"
 	"github.com/go-logr/logr"
 	"github.com/klauspost/cpuid/v2"
 	"github.com/shirou/gopsutil/v3/host"
@@ -21,5 +22,9 @@ func getHardwareData(ctx context.Context, log logr.Logger, t *Trie) {
 	t.Insert(cpuid.CPU.BrandName, "Hardware", "CPU", "Model")
 	t.Insert(strconv.Itoa(cpuid.CPU.PhysicalCores), "Hardware", "CPU", "Cores")
 	t.Insert(strconv.Itoa(cpuid.CPU.LogicalCores), "Hardware", "CPU", "Threads")
+	t.Insert(units.BytesSize(float64(cpuid.CPU.Cache.L1D)), "Hardware", "CPU", "Cache", "L1D")
+	t.Insert(units.BytesSize(float64(cpuid.CPU.Cache.L1I)), "Hardware", "CPU", "Cache", "L1I")
+	t.Insert(units.BytesSize(float64(cpuid.CPU.Cache.L2)), "Hardware", "CPU", "Cache", "L2")
+	t.Insert(units.BytesSize(float64(cpuid.CPU.Cache.L3)), "Hardware", "CPU", "Cache", "L3")
 	t.Insert(is.VirtualizationSystem+" "+is.VirtualizationRole, "Hardware", "Virtualisation")
 }

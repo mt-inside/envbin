d5f2396930bc811a839d62a52236b56e4c96cbac
Matt Turner <user.personal@users.noreply.github.com>
2021-03-15 20:03:56 +0000

    Actually put DMI data in the trie

--- pkg/data/dmi.go
+++ pkg/data/dmi.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/dselans/dmidecode"
 	"github.com/go-logr/logr"
@@ -20,58 +21,50 @@ func getDmiData(ctx context.Context, log logr.Logger, t *Trie) {
 	}
 
 	syss, _ := dmi.SearchByType(1) // system info
-	for _, sys := range syss {
-		log.Info(
-			"System",
-			"Manufacturer", sys["Manufacturer"],
-			"Product", sys["Product Name"],
-		)
+	if len(syss) != 1 {
+		log.Info("Unexpectedly many DMI readings; skipping")
+		return
 	}
+	sys := syss[0]
+	t.Insert(sys["Manufacturer"], "Hardware", "System", "Manufacturer")
+	t.Insert(sys["Product Name"], "Hardware", "System", "Product")
 
 	mbs, _ := dmi.SearchByType(2) // mobo
-	for _, mb := range mbs {
-		log.Info(
-			"Baseboard",
-			"Manufacturer", mb["Manufacturer"],
-			"Product", mb["Product Name"],
-		)
+	if len(mbs) != 1 {
+		log.Info("Unexpectedly many DMI readings; skipping")
+		return
 	}
+	mb := mbs[0]
+	t.Insert(mb["Manufacturer"], "Hardware", "Motherboard", "Manufacturer")
+	t.Insert(mb["Product Name"], "Hardware", "Motherboard", "Product")
 
 	fws, _ := dmi.SearchByType(0) // firmware
-	for _, fw := range fws {
-		log.Info(
-			"Firmware",
-			"Manufacturer", fw["Vendor"],
-			"Version", fw["Version"],
-			"Revision", fw["BIOS Revision"],
-			"Date", fw["Release Date"],
-			"ROM Size", fw["ROM Size"],
-		)
+	if len(fws) != 1 {
+		log.Info("Unexpectedly many DMI readings; skipping")
+		return
 	}
+	fw := fws[0]
+	t.Insert(fw["Manufacturer"], "Hardware", "Firmware", "Manufacturer")
+	t.Insert(fw["Product Name"], "Hardware", "Firmware", "Version")
+	t.Insert(fw["BIOS Revision"], "Hardware", "Firmware", "Revision")
+	t.Insert(fw["Release Date"], "Hardware", "Firmware", "Date")
+	t.Insert(fw["ROM Size"], "Hardware", "Firmware", "ROM Size")
 
 	cpus, _ := dmi.SearchByType(4) // CPUs
-	for _, cpu := range cpus {
-		log.Info(
-			"CPU",
-			"Socket", cpu["Socket Designation"],
-			"Max Speed", cpu["Max Speed"],
-			"Cores", cpu["Core Count"],
-			"Threads", cpu["Thread Count"],
-		)
+	for i, cpu := range cpus {
+		t.Insert(cpu["Socket Designation"], "Hardware", "CPU", strconv.Itoa(i), "Socket")
+		t.Insert(cpu["Max Speed"], "Hardware", "CPU", strconv.Itoa(i), "Max Speed")
 	}
 
 	dimms, _ := dmi.SearchByType(17) // DIMMs
-	for _, dimm := range dimms {
-		log.Info(
-			"RAM",
-			"Channel", dimm["Bank Locator"],
-			"Slot", dimm["Locator"],
-			"Ranks", dimm["Rank"],
-			"Size", dimm["Size"],
-			"Speed", dimm["Speed"],
-			"Type", dimm["Type"],
-			"Sub Type", dimm["Type Detail"],
-		)
+	for i, dimm := range dimms {
+		t.Insert(dimm["Bank Locator"], "Hardware", "Memory", strconv.Itoa(i), "Channel")
+		t.Insert(dimm["Locator"], "Hardware", "Memory", strconv.Itoa(i), "Slot")
+		t.Insert(dimm["Rank"], "Hardware", "Memory", strconv.Itoa(i), "Ranks")
+		t.Insert(dimm["Size"], "Hardware", "Memory", strconv.Itoa(i), "Size")
+		t.Insert(dimm["Speed"], "Hardware", "Memory", strconv.Itoa(i), "Speed")
+		t.Insert(dimm["Type"], "Hardware", "Memory", strconv.Itoa(i), "Type")
+		t.Insert(dimm["Type Detail"], "Hardware", "Memory", strconv.Itoa(i), "Sub Type")
 	}
 
 	// for _, record := range dmi.Data {

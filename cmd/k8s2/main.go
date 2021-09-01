5bfc8657e488681b5f545f5c5aecf291d15f2351
Matt Turner <user.personal@users.noreply.github.com>
2021-09-01 03:55:01 +0100

    WIP CLI to print k8s cluster info

---
+++ cmd/k8s2/main.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/go-logr/logr"
+	"github.com/kr/pretty"
+	"github.com/mt-inside/go-usvc"
+	"github.com/urfave/cli/v2"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func init() {
+	spew.Config.DisableMethods = true
+	spew.Config.DisablePointerMethods = true
+}
+
+func main() {
+	log := usvc.GetLogger(true)
+
+	kubeConfigPath := ""
+	if home := homedir.HomeDir(); home != "" {
+		kubeConfigPath = filepath.Join(home, ".kube", "config")
+		if _, err := os.Stat(kubeConfigPath); err != nil {
+			kubeConfigPath = ""
+		}
+	}
+
+	app := cli.App{
+		Name:  "kubeinspect",
+		Usage: "Dump info about a k8s cluster",
+
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "kubeconfig",
+				Usage: "Absolute path to kubeconfig file",
+				Value: kubeConfigPath,
+			},
+			&cli.StringFlag{
+				Name:  "api",
+				Usage: "URL of the Kubernetes API. Overrides any value in the kubeconfig.",
+			},
+		},
+
+		Action: appMain,
+
+		Metadata: map[string]interface{}{
+			"log": log,
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Error(err, "Error during exeuction")
+	}
+}
+
+func appMain(c *cli.Context) error {
+	log := c.App.Metadata["log"].(logr.Logger)
+
+	k8s, err := getClientSet(log, c.String("kubeconfig"), c.String("url"))
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	nodes, err := k8s.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+	// _, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
+	// if errors.IsNotFound(err) {
+	// 	fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+	// } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+	// 	fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+	// } else if err != nil {
+	// 	panic(err.Error())
+	// } else {
+	// 	fmt.Printf("Found example-xxxxx pod in default namespace\n")
+	// }
+
+	version, err := k8s.Discovery().ServerVersion()
+	if err != nil {
+		return fmt.Errorf("Can't get cluster version: %w", err)
+	}
+	fmt.Printf("Kubernetes %s %s", version.GitVersion, version.Platform)
+	fmt.Println()
+
+	cores := &resource.Quantity{}
+	ram := &resource.Quantity{}
+	disk := &resource.Quantity{}
+	scratch := &resource.Quantity{}
+	for _, n := range nodes.Items {
+		cores.Add(*n.Status.Capacity.Cpu())
+		ram.Add(*n.Status.Capacity.Memory())
+		disk.Add(*n.Status.Capacity.Storage())
+		scratch.Add(*n.Status.Capacity.StorageEphemeral())
+	}
+	ram.RoundUp(resource.Giga)
+	fmt.Printf("%d Nodes (%v cores, %v ram, %v disk, %v scratch)", len(nodes.Items), cores, ram, disk, scratch)
+	fmt.Println()
+
+	// node-role.kubernetes.io/control-plane":""
+	// "kubernetes.io/os":"linux"
+	// "node.kubernetes.io/exclude-from-external-load-balancers":""
+	// "kubernetes.io/arch":"amd64"
+	// "node-role.kubernetes.io/master":""
+
+	fmt.Print("Roles: ")
+	pretty.Print(groupByRole(nodes))
+	fmt.Println()
+
+	// https://kubernetes.io/docs/reference/labels-annotations-taints/
+	// All nodes
+	render(nodes, "OSen", "kubernetes.io/os")
+	render(nodes, "Arches", "kubernetes.io/arch")
+
+	// Auto-taints
+	render(nodes, "Not Ready", "node.kubernetes.io/not-ready")
+	render(nodes, "Unreachable", "node.kubernetes.io/unreachable")
+	render(nodes, "Unschedulable", "node.kubernetes.io/unschedulable")
+	render(nodes, "Memory Pressure", "node.kubernetes.io/memory-pressure")
+	render(nodes, "Disk Pressure", "node.kubernetes.io/disk-pressure")
+	render(nodes, "Network Unavailable", "node.kubernetes.io/network-unavailable")
+	render(nodes, "PID Pressure", "node.kubernetes.io/pid-pressure")
+
+	// Cloud instances
+	render(nodes, "Instance Types", "node.kubernetes.io/instance-type")
+	render(nodes, "Regions", "topology.kubernetes.io/region")
+	render(nodes, "Zones", "topology.kubernetes.io/zone")
+
+	return nil
+}
+
+func render(nodes *corev1.NodeList, title string, label string) {
+	m := groupByLabel(nodes, label)
+
+	if len(m) != 0 {
+		fmt.Printf("%s: ", title)
+		pretty.Print(m)
+		fmt.Println()
+	}
+}
+
+func groupByRole(ns *corev1.NodeList) (ret map[string]int) {
+	ret = make(map[string]int)
+
+	for _, n := range ns.Items {
+		roles := getSufficies(n.Labels, "node-role.kubernetes.io/")
+		for _, role := range roles {
+			ret[role] = ret[role] + 1
+		}
+	}
+
+	return
+}
+
+func getSufficies(ss map[string]string, pre string) []string {
+	ret := []string{}
+
+	for k := range ss {
+		if strings.HasPrefix(k, pre) {
+			ret = append(ret, strings.TrimPrefix(k, pre))
+		}
+	}
+	return ret
+}
+
+func groupByLabel(ns *corev1.NodeList, key string) (ret map[string]int) {
+	ret = make(map[string]int)
+
+	for _, n := range ns.Items {
+		val := n.Labels[key]
+		if val != "" {
+			ret[val] = ret[val] + 1
+		}
+	}
+
+	return
+}
+
+func getClientSet(log logr.Logger, kubeConfigPath string, masterURL string) (kubernetes.Interface, error) {
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeConfigPath)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting kubeconfig: %w", err)
+	}
+
+	kubeClientSet, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("Error building kubernetes clientset: %w", err)
+	}
+
+	return kubeClientSet, nil
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.1.1
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.0.2
 	github.com/cloudfoundry/gosigar v1.1.0
+	github.com/davecgh/go-spew v1.1.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/docker/go-units v0.4.0
 	github.com/dselans/dmidecode v0.0.0-20180814053009-65c3f9d81910
@@ -21,16 +22,16 @@ require (
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/joho/godotenv v1.3.0
 	github.com/klauspost/cpuid/v2 v2.0.4
+	github.com/kr/pretty v0.2.0
 	github.com/mattn/go-isatty v0.0.12
 	github.com/mt-inside/go-usvc v0.0.3
 	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f
-	github.com/onsi/ginkgo v1.12.0 // indirect
-	github.com/onsi/gomega v1.9.0 // indirect
 	github.com/shirou/gopsutil/v3 v3.21.2
 	github.com/spf13/viper v1.8.1
 	github.com/stretchr/testify v1.7.0
 	github.com/urfave/cli/v2 v2.3.0
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007
-	k8s.io/apimachinery v0.20.4
-	k8s.io/client-go v0.20.4
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22
+	k8s.io/api v0.22.1
+	k8s.io/apimachinery v0.22.1
+	k8s.io/client-go v0.22.1
 )

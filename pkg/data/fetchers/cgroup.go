02674f16b80a6e7234357bb33ea64b20d428b71b
Matt Turner <m.turner@mwam.com>
2021-10-04 16:56:54 +0100

    Add detection of cgroup support

---
+++ pkg/data/fetchers/cgroup.go
@@ -0,0 +1,50 @@
+package fetchers
+
+import (
+	"bufio"
+	"context"
+	"os"
+	"strings"
+
+	"github.com/go-logr/logr"
+	"github.com/mt-inside/envbin/pkg/data"
+	. "github.com/mt-inside/envbin/pkg/data/trie"
+)
+
+func init() {
+	data.RegisterPlugin(getCgroupData)
+}
+
+func getCgroupData(ctx context.Context, log logr.Logger, t *Trie) {
+	fsen, err := os.Open("/proc/filesystems")
+	if err != nil {
+		t.Insert(Error(err), "OS", "Isolation", "CGroups")
+		return
+	}
+	defer fsen.Close()
+
+	fsen_scanner := bufio.NewScanner(fsen)
+	for fsen_scanner.Scan() {
+		if strings.HasSuffix(fsen_scanner.Text(), "cgroup") {
+			t.Insert(Some("Yes"), "OS", "Isolation", "CGroups", "v1", "Supported")
+		} else if strings.HasSuffix(fsen_scanner.Text(), "cgroup2") {
+			t.Insert(Some("Yes"), "OS", "Isolation", "CGroups", "v2", "Supported")
+		}
+	}
+
+	mounts, err := os.Open("/proc/mounts")
+	if err != nil {
+		t.Insert(Error(err), "OS", "Isolation", "CGroups")
+		return
+	}
+	defer mounts.Close()
+
+	mounts_scanner := bufio.NewScanner(mounts)
+	for mounts_scanner.Scan() {
+		if strings.HasPrefix(mounts_scanner.Text(), "cgroup") {
+			t.Insert(Some("Yes"), "OS", "Isolation", "CGroups", "v1", "Enabled")
+		} else if strings.HasPrefix(mounts_scanner.Text(), "cgroup2") {
+			t.Insert(Some("Yes"), "OS", "Isolation", "CGroups", "v2", "Enabled")
+		}
+	}
+}
